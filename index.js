1d4790a1e60b9c925d17ee75ea8a59e52a31f81f
mohammadreza <m.rpaknazar@gmail.com>
2022-03-30 18:12:56 +0300

    until border work and solved

---
+++ index.js
@@ -0,0 +1,5 @@
+    
+document.getElementById("red").style.color="red";
+document.getElementById("blue").style.color="blue";
+document.getElementById("myDIV").style.backgroundColor ="pink";
+document.getElementById("myDIV").style.borderBlockEndColor= "black";
